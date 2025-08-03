6df021abe12514f8583902d959b4b912ab02712c
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-08-03 12:14:04 +0600

    problem 8 solve

--- problem-7.ts
+++ problem-7.ts
@@ -2,8 +2,25 @@
     // Create a TypeScript class Car with properties make, model, and year. Include a method getCarAge that returns the car's age based on the current year.
 
     class Car {
-        
+        make: string;
+        model: string;
+        year: number;
+
+        constructor(make: string, model: string, year: number) {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+        getCarAge() {
+            const totalYear = new Date().getFullYear() - this.year;
+            return totalYear;
+        }
     }
+
+    // Sample Input 1:
+    const car = new Car("Honda", "Civic", 2018);
+    const carAge = car.getCarAge();
+    console.log(carAge)
 
     //
 }
--- problem-8.ts
+++ problem-8.ts
@@ -1,4 +1,27 @@
 {
     // Create a function validateKeys(obj: T, keys: (keyof T)[]) that takes an object obj and an array of keys keys. The function should return true if all of the specified keys exist in the object; otherwise, it should return false.
+
+    type Person = {
+        name: string;
+        age: number;
+        email: string
+    }
+    const validateKeys = (param: Person, property: (keyof Person)[]): boolean => {
+        for (const key of property) {
+            if (!(key in param)) {
+                return false;
+            }
+        }
+        return true;
+    };
+    // return property.every((key) => key in param);
+
+    // Sample Input:
+    const person: Person = { name: "Alice", age: 25, email: "alice@example.com" };
+    const output: boolean = validateKeys(person, ["name", "age"]);
+    console.log(output);
+
+    // Sample Output:
+    // true;
     //
 }
